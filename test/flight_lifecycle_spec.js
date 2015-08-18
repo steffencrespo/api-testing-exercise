dbbf2cd4f9e1b8bedfb185a493137ba3db3cc163
Leonardo Steffen <steffencrespo@hotmail.com>
2015-08-18 19:07:15 -0300

    adds callback methods to allow for multiple chained operations. Callback hell, Im getting sick of them

--- test/flight_lifecycle_spec.js
+++ test/flight_lifecycle_spec.js
@@ -1,5 +1,6 @@
 var frisby = require('frisby');
-frisby.create("Get an existing flight by ID")
+
+frisby.create("Create a new flight")
 	.post('http://localhost:3000/api/flights'
 		,{
 		  data_partida: "2015-10-10",
@@ -11,23 +12,67 @@ frisby.create("Get an existing flight by ID")
 		  lugares: 0,
 		  companhia: "TAM",
 		  id: 0
-		})
-	.expectStatus(200)
-	.get("http://localhost:3000/api/flights/1")
-	.expectStatus(200)
-	.put('http://localhost:3000/api/flights'
-		,{
-		  data_partida: "2015-10-10",
-		  data_chegada: "2015-10-15",
-		  numero: "1",
-		  origem: "porto alegre",
-		  destino: "manaus",
-		  duracao: 0,
-		  lugares: 0,
-		  companhia: "TAM",
-		  id: 0
-		})
-	.expectStatus(200)
-	.get("http://localhost:3000/api/flights")
+	})
+  	.afterJSON(function(json) {
+    	var flightId = json.id;
+	    frisby.create('Get the new flight by id')
+			.get("http://localhost:3000/api/flights/" + flightId)
+			.expectStatus(200)
+	    	.after(function() {
+		        frisby.create('Change an existing flight')
+		        	.put('http://localhost:3000/api/flights'
+						,{
+						  data_partida: "2015-10-10",
+						  data_chegada: "2015-10-15",
+						  numero: "1",
+						  origem: "porto alegre",
+						  destino: "manaus",
+						  duracao: 0,
+						  lugares: 0,
+						  companhia: "TAM",
+						  id: flightId
+						})
+	        	.expectStatus(200)
+	        	.after(function() {
+	        		frisby.create('Delete the flight')
+	        		.delete("http://localhost:3000/api/flights/" + flightId)
+	        		.expectStatus(204)
+	        	}).toss();
+	        }).toss();
+    }).toss();
 
-.toss();
+// var frisby = require('frisby');
+
+// frisby.create('get new cart')
+//   .post(url + '/carts')
+//   //some some expectations
+//   .afterJSON(function(json) {
+//     var cartId = json.cartId;
+
+//     frisby.create('add item to cart')
+//       .post(url + '/carts/' + cartId, {
+//         "itemId": itemId,
+//         "quantity": qty
+//       }, {
+//         json: true
+//       })
+//       //some some expectations
+//       .after(function() {
+
+//         frisby.create('edit cart')
+//           .put(url + '/carts/' + cartId, {
+//             "itemId": itemId,
+//             "quantity": qty
+//           }, {
+//             json: true
+//           })
+//           //some expectations
+//           .after(function() {
+
+//             frisby.create('clear cart')
+//               .delete(url + '/carts/' + cartId + "/items/" + itemId)
+//               //some expectations
+//               .toss();
+//         }).toss();
+//     }).toss();
+// }).toss();
